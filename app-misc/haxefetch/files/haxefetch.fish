e5d07c953201fc1161d617235b9342cc1e85c02b
ACoolioDude <stefanbeta2008@gmail.com>
2026-09-14 21:06:58 +0200

    Update haxefetch.fish

--- app-misc/haxefetch/files/haxefetch.fish
+++ app-misc/haxefetch/files/haxefetch.fish
@@ -1,6 +1,7 @@
 complete -c haxefetch -f
 
-complete -c haxefetch -s h -l help      -d 'Show help'
-complete -c haxefetch -s v -l version   -d 'Show version of Haxefetch'
-complete -c haxefetch -s c -l config    -d 'Generate new config of Haxefetch'
-complete -c haxefetch -s t -l tutorial  -d 'Show how to customise Haxefetch'
+complete -c haxefetch -s h -l help      -d 'Show help.'
+complete -c haxefetch -s v -l version   -d 'Show version of Haxefetch.'
+complete -c haxefetch -s c -l config    -d 'Generate new config of Haxefetch.'
+complete -c haxefetch -s t -l tutorial  -d 'Show how to customise Haxefetch.'
+complete -c haxefetch -s s -l supported -d 'Haxefetch is supported.'
